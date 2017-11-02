7f355d9a3358e6e3b057a26ef394c86167a91f6b
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:28:44 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 8 - More Variables
+Published: 11/1/2012 4:07:38 PM
+Link: https://www.youtube.com/watch?v=Vs3QBn65voI
+
+Description: Learn some more tricks with variables
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut8.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+//comment, this will not get processed!
+
+$boy = "Bob";
+$girl = "Sally";
+
+echo $boy . " " . $girl;
+echo "<br>";
+
+$x = 2;
+$y = 10;
+echo $x + $y;
+echo "<br>";
+
+$z = $x * $y;
+echo $z;
+
+?>
+
+</body>
+</html>
